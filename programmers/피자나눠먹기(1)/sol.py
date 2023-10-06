1b0af4b273a4d4f4fc3551fe37cf9e7d7984a255
yoonseong <zxczxc0011@naver.com>
2023-10-06 17:45:37 +0900

    programmers

---
+++ programmers/피자나눠먹기(1)/sol.py
@@ -0,0 +1,20 @@
+# 머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 
+# 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이 피자를 한 조각 이상 먹기 위해 
+# 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
+
+def solution(n):
+
+        answer = 1
+        
+        if n % 7 ==0:
+            answer = answer + (n//7) - 1
+        elif (n // 7) > 1:
+            answer = answer + (n//7)
+        else:
+            answer = 1
+
+        return answer
+
+print(solution(7))
+print(solution(1))
+print(solution(15))
